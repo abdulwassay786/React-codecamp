9d3c6fccfccf8f186e341866c3c10411cb49fa97
Abdul Wassay Ansari <abdulwassayansari786@gmail.com>
2021-07-09 12:45:52 +0500

    Code Camp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+
+const firstBook = {
+  title: "Harry Potter And The Sorcerer's Stone",
+  author: "JK Rowling",
+  img:
+  "https://images-na.ssl-images-amazon.com/images/I/91tDBrO2u9L._AC_UL210_SR210,210_.jpg"
+};
+
+const secondBook = {
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "JK Rowling",
+  img:
+  "https://m.media-amazon.com/images/I/51RDEwuovQL.jpg"
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="book">
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}/>
     </div>
   );
 }
+
+
+const Book = (props) => {
+  return (
+    
+    <section className="bookList">
+      <img className="img" src={props.img} alt="Harry Potter" />
+      <h3 className="title">{props.title}</h3>
+      <h4 className="author">{props.author.toUpperCase()}</h4>
+    </section>
+    
+  );
+};
+
+
+
 
 export default App;
